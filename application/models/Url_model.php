49376b29b86fe52ead0b69800e409de060d95816
Krishnan <krishnan.ubuntu@gmail.com>
2017-12-29 12:45:02 +0530

    cosmetic changes

--- application/models/Url_model.php
+++ application/models/Url_model.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * @author Krishnan ks@geedesk.com
+ * @author Krishnan krishnan.ubuntu@gmail.com
  *
  * @license 
  * 
